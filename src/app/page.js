632dae01b193475146788d0d0e044aa5bdd56f43
Jahid2121 <138579420+Jahid2121@users.noreply.github.com>
2024-01-19 15:34:29 +0600

    ServiceCount component updated with data

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,14 @@
 
 import Image from 'next/image'
 import Banner from './banner/Banner';
+import ServiceCount from '@/components/ui/ServiceCount/ServiceCount';
 
 
 const Home = () => {
   return (
     <div>
       <Banner></Banner>
+      <ServiceCount />
     </div>
   );
 };
