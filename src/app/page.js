09e05779fe76887bd358f6ec35a856aeb9bad35f
shohaib1996 <khanshohaibhossain@gmail.com>
2024-01-22 20:20:34 +0600

    added benifits

--- src/app/page.js
+++ src/app/page.js
@@ -2,12 +2,14 @@
 import Banner from './banner/Banner';
 import ServiceCount from '@/app/components/ui/ServiceCount/ServiceCount';
 import Testimonial from './components/testimonial/Testimonial';
+import Benefits from './components/Benefits/Benefits';
 
 const Home = () => {
   return (
     <div>
       <Banner></Banner>
       <ServiceCount />
+      <Benefits></Benefits>
       <Testimonial></Testimonial>
     </div>
   );
